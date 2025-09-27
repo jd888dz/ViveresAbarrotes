199fa6f5369f88d68a7c1d0f97b5b6e3c5c54009
jd888dz <jd8882580@gmail.com>
2025-09-26 21:36:31 -0500

    estrcutrua

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,40 @@
-import React from 'react';
-import Hero from './components/Hero';
-import About from './components/About';
-import ProductCatalog from './components/ProductCatalog';
-import OffersCarousel from './components/OffersCarousel';
-import Reviews from './components/Reviews';
-import ContactWhatsApp from './components/ContactWhatsApp';
-import FAQ from './components/FAQ';
-import Footer from './components/Footer';
-import FloatingWhatsApp from './components/FloatingWhatsApp';
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import ProductCatalog from "./components/ProductCatalog";
+import OffersCarousel from "./components/OffersCarousel";
+import Reviews from "./components/Reviews";
+import ContactWhatsApp from "./components/ContactWhatsApp";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       {/* Hero Section */}
       <Hero />
-      
+
       {/* About Section */}
       <About />
-      
-      {/* Product Catalog */}
-      <ProductCatalog />
-      
       {/* Offers Section */}
       <OffersCarousel />
-      
+
+      {/* Product Catalog */}
+      <ProductCatalog />
+
       {/* Reviews Section */}
       <Reviews />
-      
+
       {/* Contact Form */}
       <ContactWhatsApp />
-      
+
       {/* FAQ Section */}
       <FAQ />
-      
+
       {/* Footer */}
       <Footer />
-      
+
       {/* Floating WhatsApp Button */}
       <FloatingWhatsApp />
     </div>
